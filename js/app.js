9db9207e5da08b507284a0b823ee89e0673b8dc5
Scott Robertson <scottymeuk@gmail.com>
2014-10-25 11:21:16 +0100

    Spaces

--- js/app.js
+++ js/app.js
@@ -1,35 +1,16 @@
 $(document).ready(function() {
 
-    $('[data-toggle="tooltip"]').tooltip({'placement': 'top'});
+  $('[data-toggle="tooltip"]').tooltip({'placement': 'top'});
 
-    $('.collapse').on('show.bs.collapse', function() {
-        var id = $(this).attr('id');
-        $('a[href="#' + id + '"]').closest('.panel-heading').addClass('active-faq');
-        $('a[href="#' + id + '"] .panel-title span').html('<i class="fa fa-minus"></i>');
-    });
-    $('.collapse').on('hide.bs.collapse', function() {
-        var id = $(this).attr('id');
-        $('a[href="#' + id + '"]').closest('.panel-heading').removeClass('active-faq');
-        $('a[href="#' + id + '"] .panel-title span').html('<i class="fa fa-plus"></i>');
-    });
-
-    $('.sure').on('click', function() {
-
-        var link = $(this);
-        var message = $(this).data('message') || 'Are you sure?';
-
-        bootbox.confirm(message, function(result) {
-            if (result) {
-                window.location.href = link.attr('href');
-            }
-        });
-
-        event.preventDefault();
-        return false;
-    });
+  $('.collapse').on('show.bs.collapse', function() {
+    var id = $(this).attr('id');
+    $('a[href="#' + id + '"]').closest('.panel-heading').addClass('active-faq');
+    $('a[href="#' + id + '"] .panel-title span').html('<i class="fa fa-minus"></i>');
+  });
+  $('.collapse').on('hide.bs.collapse', function() {
+    var id = $(this).attr('id');
+    $('a[href="#' + id + '"]').closest('.panel-heading').removeClass('active-faq');
+    $('a[href="#' + id + '"] .panel-title span').html('<i class="fa fa-plus"></i>');
+  });
 
 });
-
-var bithost = {
-
-}
